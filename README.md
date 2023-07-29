b62833ad0df02c3e39dff1639289d95277f56385
DiegoGRoldán <ekrnal9@gmail.com>
2023-07-29 16:06:50 -0600

    Terminamos fs, con callbacks y empezamos con promises

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-**Buenas practicas del poryecto**
+# Buenas practicas del poryecto
+
 -.js --> por defecto usa CommonJS
+
 -.mjs --> para utilizar ES Modules
+
 -.cjs para utilizar CommonJS
+
+version de node: 18.16.0
+
 -- a partir de Node 16, se recomienda poner node:
 
 --se Usará en este proyecto CommonJS
---
+++ fs-promises.js
@@ -0,0 +1,14 @@
+const fs = require('node:fs')
+
+console.log('leyendo el primer archivo  ...')
+fs.readFile('./README.md', 'utf-8', (err, text) => {
+    console.log(text)
+})
+
+
+console.log('--------------------------------')
+
+console.log('leyendo el segundo archivo')
+fs.readFile('./fs.js', 'utf-8', (err, text) => {
+    console.log(text)
+})
