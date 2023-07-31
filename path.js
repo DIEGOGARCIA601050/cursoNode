1191f1ec8ea958c8da1e903fa60350f0a59dcbff
DiegoGRoldán <ekrnal9@gmail.com>
2023-07-30 19:13:23 -0600

    Terminamos path

--- path.js
+++ path.js
@@ -4,6 +4,19 @@ console.log(path.sep)
 
 //unir rutas con path.join
 const filePath = path.join('cursoNode', 'OS', 'KB_MB_GB.js')
-console.log(filePath)
 const base = path.basename(filePath)
+const fileName = path.basename(filePath, '.js')
+const extension = path.extname('mi.primer.modulo.js')
+const ruta = {
+    root: 'C:\\',
+    dir: filePath,
+    base,
+    name: fileName,
+    ext:extension
+}
+Object.freeze(ruta)
+console.log(filePath)
 console.log(base)
+console.log(fileName)
+console.log(extension)
+console.log(path.format(ruta))
