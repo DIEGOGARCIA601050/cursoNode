3151c2a50bade46a9300003f9c5dc8dd33ee6e95
DiegoGRoldán <ekrnal9@gmail.com>
2023-09-09 18:08:21 -0600

    Legibilidad de la trnsferencia de información

--- Personal projects/index.mjs
+++ Personal projects/index.mjs
@@ -1,24 +1,42 @@
 import { recorrerObjs } from "./recorrerObjs.mjs";
 import { Persona } from "./clasePersona.mjs";
-import { writeFile } from 'node:fs/promises'
+import { writeFile } from 'node:fs/promises';
 
-const Maria = new Persona('Maria', 'n',18);
-const Pablo = new Persona('Pablo', 'Gutierres',37);
-const John = new Persona('John', 'Adams',38);
+// Crear instancias de personas
+// const Maria = new Persona('Maria', 'n', 18);
+const Pablo = new Persona('Pablo', 'Gutierres', 37);
+const John = new Persona('John', 'Adams', 38);
 
-const Mobj = recorrerObjs(Maria)
-console.log('maria',Mobj)
-const SM = Mobj.toString()
-console.log(SM)
+// Obtener información de María
+// const Mobj = recorrerObjs(Maria);
+// const MariaInfo = Mobj.toString();
 
-console.log('-------------------')
-recorrerObjs(Pablo)
-console.log('-------------------')
-recorrerObjs(John)
+// // Imprimir información de María
+// console.log('maria', Mobj);
+// console.log(MariaInfo);
+// console.log('-------------------');
 
-writeFile('./datos.txt',SM)
+// Obtener información de Pablo
+const PabloI = recorrerObjs(Pablo);
+const PabloInfo = PabloI.toString();
+
+// Obtener información de John
+const JohnI = recorrerObjs(John);
+const JohnInfo = JohnI.toString();
+
+// Escribir información de María, Pablo y John en el archivo datos.txt
+writeFile('./datos.txt', `${PabloInfo}\n`)
+  .then(() => {
+    console.log('Archivo escrito exitosamente');
+  })
+  .catch((err) => {
+    console.error(`Error al escribir archivo: ${err}`);
+  });
+
+
+writeFile('./datos.txt',`${JohnInfo}`)
 .then(() => {
-    console.log('Archivo escrito exitosamente',SM);
+    console.log('Archivo escrito exitosamente');
   })
   .catch((err) => {
     console.error(`Error al escribir archivo: ${err}`);
