003ab83163cf9ec3cda77455ad22867eca5b9b8d
DiegoGRoldán <ekrnal9@gmail.com>
2023-09-09 18:30:54 -0600

    Lograda a medias la legibilidad

--- Personal projects/index.mjs
+++ Personal projects/index.mjs
@@ -3,18 +3,14 @@ import { Persona } from "./clasePersona.mjs";
 import { writeFile } from 'node:fs/promises';
 
 // Crear instancias de personas
-// const Maria = new Persona('Maria', 'n', 18);
+const Maria = new Persona('Maria', 'n', 18);
 const Pablo = new Persona('Pablo', 'Gutierres', 37);
 const John = new Persona('John', 'Adams', 38);
 
 // Obtener información de María
-// const Mobj = recorrerObjs(Maria);
-// const MariaInfo = Mobj.toString();
+const Mobj = recorrerObjs(Maria);
+const MariaInfo = Mobj.toString();
 
-// // Imprimir información de María
-// console.log('maria', Mobj);
-// console.log(MariaInfo);
-// console.log('-------------------');
 
 // Obtener información de Pablo
 const PabloI = recorrerObjs(Pablo);
@@ -24,20 +20,13 @@ const PabloInfo = PabloI.toString();
 const JohnI = recorrerObjs(John);
 const JohnInfo = JohnI.toString();
 
+
 // Escribir información de María, Pablo y John en el archivo datos.txt
-writeFile('./datos.txt', `${PabloInfo}\n`)
-  .then(() => {
+(async () => {
+  try {
+    await writeFile('./datos.txt', `${MariaInfo}\n${PabloInfo}\n${JohnInfo}\n`);
     console.log('Archivo escrito exitosamente');
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error(`Error al escribir archivo: ${err}`);
-  });
-
-
-writeFile('./datos.txt',`${JohnInfo}`)
-.then(() => {
-    console.log('Archivo escrito exitosamente');
-  })
-  .catch((err) => {
-    console.error(`Error al escribir archivo: ${err}`);
-  });
+  }
+})();
