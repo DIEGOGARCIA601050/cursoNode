c3a2c899651151d6a6541b317b8feff7379dbb57
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2026-01-07 13:10:42 -0600

    Refactor Promise.all to iterate over results
    Transform the Promises.all to iterate over the results regardless of the number of elements

--- fs-async-await-parallel.js
+++ fs-async-await-parallel.js
@@ -7,7 +7,11 @@ const { readFile } = require('node:fs/promises');
 Promise.all([
   readFile('./README.md', 'utf-8'),
   readFile('./fs.js', 'utf-8')
-]).then(([text, segundotexto])=>{
-  console.log('1ero:',text)
-  console.log('2do:',segundotexto)
+]).then((promises)=>{
+  if(Array.isArray(promises))
+  {
+    for(i=0;i<promises.length;i++)
+    {
+      console.log(`i°: ${promises[i]}`)
+    }
 })
